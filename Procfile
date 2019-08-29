9f387a47a61521979fce959e54ff9b945394dd8f
unknown <girmayonatan86@gmail.com>
2019-08-29 17:25:10 +0300

    first commit

---
+++ Procfile
@@ -0,0 +1,5 @@
+bot: python bot.py -t 911777415:AAFbhQwHyOvEPi1wQTX-7FmafFMZP7Yso-s -i 913037 -q 4b45a047a74abbeedd60419241c3aee9
+
+
+with open('bot.py', 'r') as file:
+	print(file)
---
+++ bot.py
@@ -0,0 +1,68 @@
+from telethon import TelegramClient, events, utils
+from telethon.tl.types import InputBotInlineResult
+from datetime import datetime
+import logging, argparse, os, subprocess
+
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',level=logging.WARNING)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--TOKEN', required=True)
+parser.add_argument('-i', '--api_id', type=int, required=True)
+parser.add_argument('-q', '--api_hash', required=True)
+args = parser.parse_args()
+
+TOKEN = args.TOKEN
+api_id = args.api_id
+api_hash = args.api_hash
+
+bot = TelegramClient('pythoncompiler', api_id, api_hash).start(bot_token=TOKEN)
+
+
+@bot.on(events.NewMessage)
+async def start(event):
+    chat = await event.get_sender()
+    async with bot.conversation(chat) as conv:
+        await conv.send_message('Python 3.7.4 ({} {})\n >>> send code here'.format(chat.username, str(datetime.now())))
+            
+        code = await conv.get_response()
+        if code.text:
+            name = str(chat.id)+'.py'
+            with open(name, 'w') as main:
+                main.write(code.text)
+
+            command = subprocess.Popen(['python', name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            (output, error) = command.communicate()
+            if error:
+                await conv.send_message('>>>\n{}'.format(error.decode('utf-8')))
+            else:
+                await conv.send_message('>>>\n{}'.format(output.decode('utf-8')))
+
+
+
+@bot.on(events.InlineQuery)
+async def inlinehandler(event):
+    builder = event.builder
+    chat = await event.get_sender()
+    name = str(chat.id)+'.py'
+        
+    with open(name, 'w') as main:
+        main.write("import builtins, os\n")
+        main.write("builtins.open = lambda x, y: print('NameError: name open is not defined')\n")
+        main.write(event.text + '\n')
+        
+    command = subprocess.Popen(['python', name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (output, error) = command.communicate()
+    if 'os' in event.text or 'sys' in event.text:
+        await event.answer([builder.article('see output', text='>>>\n{}'.format("\U0001F610"))])
+    else: 
+        if error:
+            await event.answer([builder.article('see output', text='>>>\n{}'.format(error.decode('utf-8')))])
+        else:
+            await event.answer([builder.article('see output', text='>>>\n{}'.format(output.decode('utf-8')))])
+
+
+        
+bot.run_until_disconnected()
+
+
